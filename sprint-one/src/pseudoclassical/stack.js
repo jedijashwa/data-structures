08bb6c2876a6b683c50ab2ab09cadc5ba42f34e5
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-14 12:21:47 -0700

    Made count variable and set size to return that.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,12 +1,15 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
 
 Stack.prototype.size = function() {
-  return 0;
+  return this.count;
 };
+
+
 
 
 
