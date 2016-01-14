ebb576bd6077c8e32375e49a0fa77959f1a0d8e3
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-13 17:00:36 -0700

    Created empty methods for queue and dequeue and created size method.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,24 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var result = {count: 0, storage: {}}; 
+  _.extend(result, queueMethods);
+  
+  return result;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function () {
+    return this.count;
+  },
+  
+  queue: function () {
+    
+  },
+  
+  dequeue: function () {
+    
+  }
+};
 
 
