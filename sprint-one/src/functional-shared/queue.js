0ceac5602b29502d84c2bf4c5653e2d456902377
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-13 17:04:20 -0700

    Added enqueue and dequeue counts and enqueue method.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,7 +1,12 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var result = {count: 0, storage: {}}; 
+  var result = {
+    count: 0,
+    enqueueCount: 1,
+    dequeueCount: 1,
+    storage: {}
+  }; 
   _.extend(result, queueMethods);
   
   return result;
@@ -12,8 +17,10 @@ var queueMethods = {
     return this.count;
   },
   
-  queue: function () {
-    
+  enqueue: function (value) {
+    this.count++;
+    this.storage[this.enqueueCount] = value;
+    this.enqueueCount++;
   },
   
   dequeue: function () {
