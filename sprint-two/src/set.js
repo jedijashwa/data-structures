a4a7a8a15e5148155c987737d7a8af474fe09217
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-01-16 11:15:08 -0700

    Made set use a HashTable instead of an object.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,28 +1,28 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = {};
+  set._storage = new HashTable();
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
-  this._storage[item] = item;
+  this._storage.insert(item, item);
 };
 
 setPrototype.contains = function(item){
-  return !!this._storage[item];
+  return !!this._storage.retrieve(item);
 };
 
 setPrototype.remove = function(item){
-  delete this._storage[item];
+  delete this._storage.remove(item);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  *
- * add()    : O(1)
- * contain(): O(1)
- * remove() : O(1)
+ * add()     : O(1)
+ * contain() : O(1)
+ * remove()  : O(1)
  *
  */
